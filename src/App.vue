14c53b65f5da6110d2bf7280bd8676819c570cbe
Phakapol Chuliang <phakapol.chuliang@gmail.com>
2020-07-08 20:14:13 +0700

    integrate with line url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-app id="app">
-    <p>{{profile.userId}}</p>
+		<p>{{ profile.userId }}</p>
 		<router-view />
 		<bottomNav />
 	</v-app>
@@ -23,23 +23,26 @@ export default {
 		BottomNav: () => import("./components/BottomNav"),
 	},
 	beforeCreate() {
-    this.$liff.init({ liffId: "1654423501-J2xEM1zY" })
-    .then(() => {
-      if (!this.$liff.isLoggedIn()) {
-        this.$liff.login();
+		this.$liff
+			.init({ liffId: "1654423501-J2xEM1zY" })
+			.then(() => {
+				this.$liff.ready.then(() => {
+					if (!this.$liff.isLoggedIn()) {
+						this.$liff.login();
+						this.getProfile();
+					} else {
+						this.getProfile();
+					}
+				});
+			})
+			.catch(function(err) {
+				// eslint-disable-next-line no-console
+				console.log("Error is: " + err);
+			});
+	},
+	mounted() {
 		this.getProfile();
-      } else {
-		this.getProfile();
-	}
-    })
-    .catch(function(err) {
-      // eslint-disable-next-line no-console
-      console.log("Error is: " + err);
-    });
-  },
-  mounted() {
-    this.getProfile();
-  },
+	},
 	methods: {
 		getProfile() {
 			let _this = this;
