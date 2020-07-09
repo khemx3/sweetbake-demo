b1037516490eb6cc922da37a3aaeeb034a342024
Phakapol Chuliang <phakapol.chuliang@gmail.com>
2020-07-09 15:43:16 +0700

    menu styling

--- src/components/ordercard.vue
+++ src/components/ordercard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ma-2 mb-15">
+    <div class="ma-2 mb-15 justify-center">
         <v-row >
             <v-col cols="12" sm="6" md="4" lg="3" v-for="item in openorder" :key="item.id">
                 <v-card class="text-center " color="#b4a78d">
@@ -11,17 +11,20 @@
                 </v-responsive>
                 <v-card-text>
                     <div class="text-h6 ">{{item.name}}</div>
-                    <!-- <div class="black--text">{{item.type}}</div> -->
-                    <v-chip class="mt-2">{{item.date}}</v-chip>
+                    <v-row class="justify-center">
+                        <v-icon @click="decrease(item)">mdi-minus</v-icon>
+                        <v-chip>{{item.count}}</v-chip>
+                        <v-icon @click="increase(item)">mdi-plus</v-icon>
+                    </v-row>
+                    <div class="black--text"></div>
                 </v-card-text>
                 
                 
                 <div  @click="selectCard(item)">
                     <v-btn 
-                    to="/menu"
-                    class="mb-2" depressed color="primary"
+                    class="mb-2 white--text" depressed color="brown"
                     >
-                        สั่งเลย
+                        เพิ่มใส่ตะกร้า
                     </v-btn>
                 </div>
                 
@@ -42,17 +45,20 @@ export default {
                 {
                 id: 1,
                 name: 'Nutella Brownies',
-                url: 'https://www.img.in.th/images/1c84cdcb4935c6eb6c7c7501055d170d.jpg'
+                url: 'https://www.img.in.th/images/1c84cdcb4935c6eb6c7c7501055d170d.jpg',
+                count: 0
                 },
                 {
                 id: 2,
                 name: 'Nutella Cup',
-                url: 'https://www.img.in.th/images/541999340d24c09282bc9130d2c78752.jpg'
+                url: 'https://www.img.in.th/images/541999340d24c09282bc9130d2c78752.jpg',
+                count: 0
                 },
                 {
                 id: 3,
                 name: 'Combo Set',
-                url: 'https://www.img.in.th/images/6be5c9719af116c54434c91f071b6c06.jpg'
+                url: 'https://www.img.in.th/images/6be5c9719af116c54434c91f071b6c06.jpg',
+                count: 0
                 }
             ]
         }
@@ -60,6 +66,14 @@ export default {
     methods: {
         selectCard: function(item) {
             this.$store.commit('SET_ORDERCARD', item)
+        },
+        increase: function(item){
+            item.count++;
+        },
+        decrease: function(item){
+            if(item.count > 0) {
+                item.count--;
+            }
         }
     }
 }
